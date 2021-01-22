85957555e94a2c8211c9cfd96efb202e3208be4d
min00young <68951739+min00young@users.noreply.github.com>
2021-01-22 16:38:13 +0900

    Modify: settings.py
    이메일주소 및 비밀번호 내용수정

--- o9o9/settings.py
+++ o9o9/settings.py
@@ -155,5 +155,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tmdwprpdla@gmail.com'
-EMAIL_HOST_PASSWORD = 'tmdwp0336'
+EMAIL_HOST_USER = 'emailaddress'
+EMAIL_HOST_PASSWORD = 'password'
